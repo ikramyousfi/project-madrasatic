e75c0babb72e69f38dba714b6e22ec772979e964
Chaima SEHILI <c.sehili@esi-sba.dz>
2022-05-16 13:55:00 +0100

    adds models registration to admin.py to be viewed in admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from roles.models import *
+from users.models import *
 
-# Register your models here.
+# Register roles app models
+@admin.register(permissions)
+class PermissionsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+# Register users app models
+@admin.register(UserManager)
+class UserManagerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
