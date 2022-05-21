234b43478ef7332b870a723d07ec3ca11bae7319
AyoubFrihaoui <a.frihaoui@esi-sba.dz>
2022-05-21 17:55:01 +0100

    roles update added

--- roles/models.py
+++ roles/models.py
@@ -3,6 +3,8 @@ from django.db import models
 import sys
 sys.path.append('../')
 from users.models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt
 
 
 
@@ -20,6 +22,26 @@ class permissions(models.Model):
     EditCategory        = models.BooleanField(default=False, null=False)
     DeleteCategory      = models.BooleanField(default=False, null=False)
     AddCategory         = models.BooleanField(default=False, null=False)
+    
+    def get_user_perm(self, request):
+        token = request.COOKIES.get('jwt')
+        
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        id = user.id
+        roles = Role.objects.filter(UserID=id).all()
+        #perms = permissions.
+
+#  def perm_add_role(self, request):
+        #id
+
 
 class Role(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, unique=True)
